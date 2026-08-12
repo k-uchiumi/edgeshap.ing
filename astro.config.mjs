78787f693c95eafdf42d4468ffbdc34d7cb4562e
内海 賢一 <kenichi@M4ProMacBookPro.local>
2026-08-12 10:21:32 +0900

    Add sitemap generation and fix robots.txt sitemap URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://edgeshap.ing',
   trailingSlash: 'never',
-  build: { format: 'file' }
+  build: { format: 'file' },
+  integrations: [sitemap()]
 });
